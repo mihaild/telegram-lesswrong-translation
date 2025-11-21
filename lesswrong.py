3098dc85524f24947bc1677da6c99306f05ef8c7
Mikhail Dektyarev <mihail.dektyarow@gmail.com>
2025-11-21 22:32:16 +0100

    Switch post requests to RSS

--- lesswrong.py
+++ lesswrong.py
@@ -1,8 +1,5 @@
-import json
-import requests
 import dataclasses
-
-LESSWRONG_JSON_PATH = 'lesswrong.json'
+import feedparser
 
 
 @dataclasses.dataclass
@@ -16,14 +13,13 @@ def get_post_url(post: dict) -> str:
     return f"https://www.lesswrong.com/posts/{post['_id']}/{post['slug']}"
 
 
-def get_last_posts(data_file: str = LESSWRONG_JSON_PATH) -> LesswrongPost:
-    config = json.load(open(data_file))
-    response = requests.post('https://www.lesswrong.com/graphql', cookies={}, headers=config['headers'], json=config['last_posts_request'])
-    j = json.loads(response.content.decode().strip())
-    posts = j[0]['data']['posts']['results']
+def get_last_posts() -> LesswrongPost:
+    feed = feedparser.parse('https://www.lesswrong.com/feed.xml?view=frontpage-rss&karmaThreshold=15')
+    if feed.status != 200:
+        raise ValueError(f'Feed error {feed!r}')
     return [LesswrongPost(
-        url=get_post_url(post),
-        title=post['title'],
-        html=post['contents']['html'],
-    ) for post in posts if post['contents']] # TODO: what are posts without content?
+        url=entry.link,
+        title=entry.title,
+        html=entry.summary,
+    ) for entry in feed.entries]
 
